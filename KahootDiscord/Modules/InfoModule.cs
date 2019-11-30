5f0b2f3eaa98dac35d875e99e401a5400fa747bb
yesiateyoursheep <yesiateyoursheep@gmail.com>
2019-11-30 23:57:10 +1300

    Rewrite in dotnet core

---
+++ KahootDiscord/Modules/InfoModule.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+
+namespace DiscordBot.Modules
+{
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("info")]
+        public Task Info()
+            => ReplyAsync(
+                embed: new EmbedBuilder() {
+                    Title = "KahootDiscord",
+                    Url = "https://kahoot.yiays.com/",
+                    Description = "Kahoot clone but on discord.",
+                    ImageUrl = "https://cdn.discordapp.com/attachments/532699822214348810/550562868458946571/PkIeN4J.png",
+                    Color = new Color(247,183,49)
+                }.Build());
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# KahootDiscord
+Website: <https://kahoot.yiays.com/>  
+Developed collaboratively by: [Duncan](https://github.com/alt1f923) and [Yiays](https://github.com/yesiateyoursheep)  
+    
+***
+    
+![PICTURE IS UNRELATED](https://cdn.discordapp.com/attachments/532699822214348810/550562868458946571/PkIeN4J.png)
